cf10a4920bbe11e3b015b24d71c11f555ba2c775
Stone Zhang <stnzhl@gmail.com>
2019-05-11 14:52:42 +0900

    Create initial macOS project

---
+++ GGObjectiveC/GGObjectiveC/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  GGObjectiveC
+//
+//  Created by Stone Zhang on 5/11/19.
+//  Copyright © 2019 Stone Zhang. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
