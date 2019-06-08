3c93ff403ae8e113fecf4bb18ca92c80b396bee0
Stone Zhang <stnzhl@gmail.com>
2019-06-08 23:03:03 +0900

    Add unique_paths

---
+++ GGObjectiveC/GGObjectiveCTests/Problems/DynamicProgramming/unique_paths/GOCUniquePathsSolution.h
@@ -0,0 +1,17 @@
+//
+//  GOCUniquePathsSolution.h
+//  GGObjectiveCTests
+//
+//  Created by Stone Zhang on 6/8/19.
+//  Copyright © 2019 Stone Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GOCUniquePathsSolution : NSObject
+- (NSInteger)uniquePathsWithM:(NSInteger)m n:(NSInteger)n;
+@end
+
+NS_ASSUME_NONNULL_END
